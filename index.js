f409424e1ed41ca826c17eae369f76286dd51dfe
Christophe Diederichs <chm-diederichs@protonmail.com>
2022-11-07 11:56:05 +0000

    rename module to autochannel

--- index.js
+++ index.js
@@ -1,255 +1,158 @@
-const { Readable } = require('streamx')
-const sodium = require('sodium-universal')
-const Corestore = require('corestore')
-const debounceify = require('debounceify')
-const Accumulator = require('accumulator-hash')
+module.exports = class Autochannel {
+  constructor (local, remote, opts = {}) {
+    this.local = local
+    this.remote = remote
 
-module.exports = class OpLog extends Readable {
-  constructor (id, opts) {
-    super()
+    this.isInitiator = Buffer.compare(local.key, remote.key) < 0
 
-    if (!id) {
-      const seed = Buffer.alloc(32)
-      sodium.randombytes_buf(seed)
-      id = seed.toString('hex')
+    this.initiator = this.isInitiator ? local : remote
+    this.responder = this.isInitiator ? remote : local
+
+    this.prev = {
+      initiator: 0,
+      responder: 0
     }
-
-    this.store = opts.store
-      ? opts.store.namespace('oplog' + id)
-      : new Corestore(opts.storage)
-
-    this.local = this.store.get({ name: 'local' })
-    this.remote = null
-
-    this.remoteIndex = 0
-
-    this._opened = false
-    this.updating = false
-    this.applying = null
-
-    this.log = this.store.get({ name: 'log' })
-
-    this._queue = []
-    this._append = debounceify(this._appendBatch.bind(this))
-
-    this.buffer = []
-
-    this._acc = new Accumulator()
   }
 
-  async loadRemote (remoteFeedKey) {
+  async ready () {
     await this.local.ready()
-    await this.log.ready()
-
-    this.remote = this.store.get(remoteFeedKey)
     await this.remote.ready()
-
-    this.emit('live')
-    this._opened = true
-
-    this._listen()
   }
 
-  // Get a deterministically ordered list of entries occuring between
-  // two given clocks. The writer/reader is needed in order to know
-  // who appended to the oplog last according to the `end` clock
-  async * getState (start, end) {
-    if (start === null && end === null) return
-
-    // deterministically choose key if end is not provided
-    const writersKey = end
-      ? end.key
-      : Buffer.compare(this.remote.key, this.local.key) > 0
-        ? this.local.key
-        : this.remote.key
-
-    const seq = order(start, writersKey)
-    const stop = end
-      ? order(end, writersKey)
-      : current.apply(this, [writersKey])
-
-    let [w, r] = [this.local, this.remote]
-    if (Buffer.compare(writersKey, this.local.key)) [w, r] = [r, w]
-
-    const wopts = { start: seq.writer, end: stop.writer }
-    const ropts = { start: seq.reader, end: stop.reader }
-
-    const writer = w.createReadStream(wopts)[Symbol.asyncIterator]()
-    const reader = r.createReadStream(ropts)[Symbol.asyncIterator]()
-
-    let left = await writer.next()
-    let right = await reader.next()
-
-    while (!left.done || !right.done) {
-      const l = decode(left.value)
-      const r = decode(right.value)
-
-      const next = left.done ? -1 : right.done ? 1 : compare(l.clock, r.clock)
-
-      if (next >= 0) {
-        yield l
-        left = await writer.next()
-      } else if (next < 0) {
-        yield r
-        right = await reader.next()
-      }
-    }
-
-    // order the clock from the writer's perspective
-    function order (clock, key, stop = false) {
-      if (!clock) return { writer: 0, reader: 0 }
-
-      const theirClock = Buffer.compare(clock.key, key)
-      const reader = theirClock ? clock.local : clock.remote
-      const writer = theirClock ? clock.remote : clock.local
-
-      return { writer, reader }
-    }
-
-    function compare (a, b) {
-      // if the reader has seen later of writers
-      if (a.local < b.remote) return 1
-      // if the writer has seen later of readers
-      if (a.remote > b.local) return -1
-      return 0
-    }
-
-    function current (key) {
-      if (Buffer.compare(key, this.local.key)) {
-        return {
-          writer: this.remote.length,
-          reader: this.local.length
-        }
-      }
-      return {
-        writer: this.local.length,
-        reader: this.remote.length
-      }
-    }
-  }
-
-  hash (data) {
-    return this._acc.hash(data)
-  }
-
-  // return -1 if the a is fully behind b
-  // return 0 if the clocks reference the same point
-  // return 1 if a references any updates that b doesn't
-  compare (a, b) {
-    if (!a) return -1
-    if (!b) return 1
-
-    const same = a.key.equals(b.key)
-
-    const bLocal = b[same ? 'local' : 'remote']
-    const bRemote = b[same ? 'remote' : 'local']
-
-    if (a.local > bLocal) return 1
-    if (a.remote > bRemote) return 1
-    if (a.local < bLocal) return -1
-    if (a.remote < bRemote) return -1
-    return 0
-  }
-
-  // returns the union of two clocks:
-  // union([1, 3], [2, 2]) -> [2, 3]
-  union (a, b) {
-    if (a == null) return b
-    if (b == null) return a
-
-    const same = !Buffer.compare(a.key, b.key)
-
-    return {
-      key: a.key,
-      local: Math.max(a.local, same ? b.local : b.remote),
-      remote: Math.max(a.remote, same ? b.remote : b.local)
-    }
-  }
-
-  isLocal (clock) {
-    return Buffer.compare(clock.key, this.local.key) === 0
-  }
-
-  _listen (signal) {
-    this.remote.on('append', getRemote.bind(this))
-
-    async function getRemote () {
-      while (this.remoteIndex < this.remote.length) {
-        const block = await this.remote.get(this.remoteIndex++)
-        this.emit('data', decode(block))
-      }
-    }
-  }
-
-  async get (clock) {
-    if (!this._opened) return // for testing
-
-    const remote = Buffer.compare(clock.key, this.local.key)
-    const feed = remote ? this.remote : this.local
-
-    return feed.get(clock.local)
-  }
-
-  async _enqueue (data) {
-    return new Promise((resolve, reject) => {
-      this._queue.push({ data, promise: { resolve, reject } })
-      this._append()
-    })
-  }
-
-  async append (type, data) {
-    if (!this._opened) return // for testing
+  async append (op, commitment = this.isInitiator) {
+    const remoteLength = this.remote.length
 
     const entry = {
-      type,
-      data,
-      clock: this.clock()
+      op,
+      commitment,
+      remoteLength
     }
 
-    const details = Buffer.from(JSON.stringify(entry))
-    entry.eventId = this.hash(details)
-
-    await this._enqueue(JSON.stringify(entry))
-
-    return entry.eventId // should we return seq as well?
+    return this.local.append(entry)
   }
 
-  _appendBatch () {
-    const batch = this._queue
-    this._queue = []
+  async * accepted () {
+    const resp = getForwardIterator(this.responder)
+    const init = this.initiator.createReadStream({ live: true })
 
-    let seq = this.local.length
+    let r = null
+    let batch = []
 
-    return this.local.append(batch.map(e => e.data)).then(() => {
-      for (const { promise } of batch) {
-        promise.resolve(seq++)
+    let lseq = 0
+    for await (const l of init) {
+      if ((r?.seq || 0) >= l.remoteLength - 1) {
+        this.prev.initiator++
+        yield l
+        continue
       }
-    })
+
+      // keep looping until we reach head
+      while (true) {
+        r = await resp.next()
+        batch.push(r.value)
+
+        if (r.value.commitment) {
+          while (batch.length) {
+            this.prev.responder++
+            yield batch.shift()
+          }
+        }
+
+        // latest is elsewhere
+        if (r.seq === l.remoteLength - 1) break
+        if (r.seq === this.responder.length - 1) break
+      }
+
+      while (batch.length) {
+        this.prev.responder++
+        yield batch.shift()
+      }
+
+      this.prev.initiator++
+      yield l
+    }
+  }
+
+  async next (prev = this.prev) {
+    const init = getReverseIterator(this.initiator, prev.initiator)
+    const resp = getReverseIterator(this.responder, prev.responder)
+
+    const batch = []
+    const heads = [[], []]
+
+    let l = await init.next()
+    let r = await resp.next()
+
+    while (!l.done || !r.done) {
+      const lstop = (r.value?.remoteLength || 0) - 1
+      const rstop = (l.value?.remoteLength || 0) - 1
+
+      const [left, right] = heads
+
+      // keep going back until we get to last seen length
+      while (!l.done && l.seq > lstop) {
+        left.push({
+          value: l.value,
+          seq: l.seq,
+          remote: this.isInitiator
+        })
+        l = await init.next()
+      }
+
+      // keep going back until we get to last seen length
+      while (!r.done && r.seq > rstop) {
+        right.push({
+          value: r.value,
+          seq: r.seq,
+          remote: !this.isInitiator
+        })
+        r = await resp.next()
+      }
+
+      // initiator first
+      if (!this.isInitiator) heads.reverse()
+      for (const head of heads) {
+        while (head.length) batch.push(head.shift())
+      }
+    }
+
+    return batch
   }
 
   clock () {
-    return {
-      key: this.local.key,
-      remote: this.remote ? this.remote.length : 0,
-      local: this.local.length
+    const start = {
+      local: this.local.length,
+      remote: this.remote.length
     }
   }
 }
 
-// yolo json encoding
-function decode (o) {
-  if (o instanceof Uint8Array) return decode(JSON.parse(o))
-  if (o == null) return null
-  if (Array.isArray(o)) return o.map(decode)
-  if (o && o.type === 'Buffer') return Buffer.from(o.data)
+function getForwardIterator (feed, start, opts) {
+  let seq = feed.length
+  const str = feed.createReadStream({ start, live: true })
+  const ite = str[Symbol.asyncIterator]()
 
-  const ret = {}
-  for (const [k, v] of Object.entries(o)) {
-    if (typeof v === 'object') {
-      if (v == null) continue
-      ret[k] = decode(v)
-    } else ret[k] = v
+  return {
+    async next () {
+      return {
+        ...(await ite.next()),
+        seq: this.done ? seq : seq++,
+      }
+    }
   }
+}
 
-  return ret
+function getReverseIterator (feed, start, opts) {
+  let seq = feed.length
+  return {
+    async next () {
+      if (seq <= start || seq === 0) return { value: null, done: true }
+      const value = await feed.get(--seq)
+      return {
+        value,
+        seq,
+        done: false
+      }
+    }
+  }
 }
